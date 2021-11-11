9535f09c61bf911f3ca1ea816f61c1bd9b6feb86
ced4github <94117026+ced4github@users.noreply.github.com>
2021-11-11 20:41:48 +0800

    Create party_app.py

---
+++ party_app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Name - party_app.py
+# Author : Cedric MALLET - cedric_mallet@hotmail.com
+# Date created 11/11/2021
+# Date updated 11/11/2021
+# Description : WEB Application with streamlit
+
+import streamlit as st
+import pandas as pd
+import os
+
+st.write('hello')
