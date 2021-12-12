6156f75dbe130bd41f3153f8aaa5d96d0ff7484c
ced4github <94117026+ced4github@users.noreply.github.com>
2021-12-12 10:30:13 +0800

    V2 avec details

--- party_app.py
+++ party_app.py
@@ -46,7 +46,7 @@ styles = [
 st.sidebar.title('Le passage à 2022')
 jour = st.sidebar.selectbox(
     "Menu",
-    ("Le lieu","Soirée du 31 Décembre", "Le 1er Janvier","Le coût")
+    ("Le lieu","Soirée du 31 Décembre", "Le 1er Janvier","Le coût","Le couchage")
 )
 
 if jour == "Le lieu":
@@ -131,58 +131,78 @@ if jour == "Le 1er Janvier":
     st.image('https://cdn.vox-cdn.com/thumbor/LDguMoaLglKzWfDoScChJPJ6F5w=/0x0:8256x5504/2570x1446/filters:focal(3456x1020:4776x2340):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70017525/1235975361.0.jpg', caption='...une thématique en vue ?')
 
 if jour == "Le coût":
-    st.title('Les frais sont partagés et un premium est appliqué le soir du réveillon (bouffe du réveillon inclue) via un algorithme intelligent... comme moi !')
+    st.title('Les frais sont désormais séparés entre le coût logement (fixés) et le coût de la bouffe qui varie encore en fonction des choix !')
     #construire le dataframe
-    df1 = pd.DataFrame({
+    dflogement = pd.DataFrame({
         'index': ['Dada','Rage','Nours','Funes','Nico','Math','Nonos','Lolo','Dom','Ced'],
-        '31 Decembre': [563,638,356,638,489,178,312,624,489,489],
-        '1er Janvier': [360,0,180,0,248,90,158,0,225,315],
+        '31 Decembre': [374,345,230,431,316,230,201,374,316,230],
+        '1er Janvier': [376,0,188,0,259,188,165,0,235,329],
         }).set_index('index')
 
-    df1b = pd.DataFrame({
+    dfbouffe = pd.DataFrame({
         'index': ['Dada','Rage','Nours','Funes','Nico','Math','Nonos','Lolo','Dom','Ced'],
-        '31 Decembre': [537,608,340,607,468,340,297,538,467,467],
-        '1er Janvier': [368,0,184,0,253,184,161,0,230,322],
+        '31 Decembre': [180,150,120,195,165,120,105,180,165,120],
+        '1er Janvier': [160,0,80,0,110,80,70,0,100,140],
         }).set_index('index')
 
-    st.subheader("New cost - based on confirmed attendees")
-    st.bar_chart(df1b)
-    st.subheader("Previous cost")
-    st.bar_chart(df1)
+    st.subheader("Lodging cost - based on confirmed attendees")
+    st.bar_chart(dflogement)
+    st.subheader("Food cost - simulation with 60 euros on 31 st for adult and 40 euros on 1er for adult")
+    st.bar_chart(dfbouffe)
     with st.expander("Voir le détail des coûts"):
         st.info("""
         Le tableau reprend la cotisation a la teuf de l'année 2021...
         ... et on va dire aussi 2022 (étant donné que cela se termine le 2 Janvier)
         """)
         st.warning("""
-        ATTENTION Prix en fonction des participants, pour le moment on joue avec 10 équipes en piste.
-
-        Ceci correspond en moyenne à ~170EUR - 1er soir avec bouffe et la literie(adulte full price), ado .75 and kid .5
+        - 31 décembre: Ceci correspond à 115EUR (adulte full price), ado .75 and kid .5
+        - 1er janvier: Ceci correspond à 94EUR (adulte full price), ado .75 and kid .5
         """)
-        st.table(df1b.style.set_table_styles(styles).set_caption("Image by Cedric le magnifi..."))
-
-    df2 = pd.DataFrame({
+        st.table(dflogement.style.set_table_styles(styles).set_caption("Image by Cedric le magnifi..."))
+        st.info("""
+        - 31 décembre: Ceci correspond à 60EUR (adulte full price), ado .75 and kid .5
+        - 1er janvier: Ceci correspond à 40EUR (adulte full price), ado .75 and kid .5
+        """)
+        st.table(dfbouffe.style.set_table_styles(styles).set_caption("Image by Cedric le magnifi..."))
+if jour == "Le couchage":
+    st.title('Les lits sont désormais répartis avec des chambres enfants/ados et les chambres adultes - Pour ceux qui se levent le 1er - pas de Rez-de chaussée)!')
+    st.warning("""
+        Il faut des matelas supplémentaires comme suivant:
+        - matelas 2 places: Nonos amène
+        - matelas 1 places: Math amène
+        - matelas 1 places: Rage amène
+        - matelas 1 places: Nours amène
+        - matelas 1 places: Dada amène
+        """)
+    dfcouchage = pd.DataFrame({
         'index': ['Adultes','Ados girls','Ados boys','Kids girls','Kids boys'],
-        'Nb personnes': [19,4,3,3,3],
+        'Nb personnes': [19,3,3,3,3],
         'Nb chambres': [9,1,1,1,1],
         }).set_index('index')
-    with st.expander("Voir la répartition des chambres en nombre"):
-        st.table(df2.style.set_table_styles(styles).set_caption("Image by Ced..."))
+    with st.expander("Voir la répartition des chambres en nombre et allocation temporaire"):
+        st.table(dfcouchage.style.set_table_styles(styles).set_caption("Image by Ced..."))
         st.warning("""
         ***Rez-de-chaussée***
-        - Les kids Boys vont avoir 1 chambre avec 1 matelas simple à ajouter
-        - Les 3 Ados boys squattent le bureau à amenager avec 1 matelas double et 1 matelas simple
-        - 1 chambre Adulte
+        - Les kids Boys vont avoir la chambre 2 avec 1 matelas simple à ajouter
+        - Math gang: chambre 1
         - Salon 2 canapés pour 2 gardiens qui devront monter la garde
+        - Les 3 Ados boys squattent le bureau ou le palier du 2eme niveau avec 1 matelas double et 1 matelas simple
 
         ***1er étage***
         - 6 chambres adultes
+            - chb3: Dom/Alex VM
+            - chb4: Dada/Alex DB
+            - chb5: Ced/Mutsu
+            - chb6: Nico/Ninie
+            - chb7: Funes/Ivy
+            - chb8: Nours/Celine
 
         ***2éme étage***
         - 2 chambres adultes
-        - Les Kids Girls vont avoir 1 chambre avec 1 matelas simple à ajouter
-        - Les Ados Girls vont avoir 1 chambre avec 2 matelas simples à ajouter
-
+            - chb9: Rage/Caro
+            - chb10: Lolo/Virginie
+        - Les Ados Girls chb11 avec 1 matelas simple à ajouter
+        - Les 3 Kids Girls chb12 avec 1 matelas simple à ajouter
         """)
 
     st.markdown('<a href="mailto:cedric_mallet@hotmail.com">Pour obtenir davantage d\'information, merci de contacter Ced</a>', unsafe_allow_html=True)
